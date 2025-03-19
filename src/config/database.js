4618ce91eb865fc4902d5d5d7c1faa9cd5e185b8
FelipeOropeza <felipe2006.co@gmail.com>
2025-03-19 20:17:08 -0300

    feat: atualizando o banco de dados pra contem produtos e manipulação de gatinhos pelo pgSql

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,24 @@
 import { PrismaClient } from "@prisma/client";
+import pkg from 'pg';
+import "dotenv/config";
 
+const { Client } = pkg;
 const prisma = new PrismaClient();
 
-export default prisma;
+const pgClient = new Client({
+  connectionString: process.env.DATABASE_URL,
+});
+
+pgClient.connect();
+pgClient.query("LISTEN new_product");
+
+pgClient.on("notification", async (msg) => {
+  if (msg.channel === "new_product") {
+    const novoProduto = JSON.parse(msg.payload);
+    console.log("🆕 Novo produto inserido:", novoProduto);
+
+    global.io.emit("updateProdutos", novoProduto);
+  }
+});
+
+export { prisma, pgClient };
